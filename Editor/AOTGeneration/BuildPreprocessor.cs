2baba97b60c62c4c718e8bb8d5ae93822b548967
Billiton Peng <62238461+BillyPung@users.noreply.github.com>
2025-02-10 17:17:10 +0800

    Update BuildPreprocessor.cs

--- Editor/AOTGeneration/BuildPreprocessor.cs
+++ Editor/AOTGeneration/BuildPreprocessor.cs
@@ -100,7 +100,7 @@
             // if IL2CPP setting and OptimizeSpeed, emit generic types usage.
 #if UNITY_2021_2_OR_NEWER
             var codeGeneration =
-    #if UNITY_2022_OR_NEWER
+    #if UNITY_2022_OR_NEWER || UNITY_6000_OR_NEWER
                 PlayerSettings.GetIl2CppCodeGeneration(NamedBuildTarget.FromBuildTargetGroup(EditorUserBuildSettings.selectedBuildTargetGroup));
     #else
                 EditorUserBuildSettings.il2CppCodeGeneration;
